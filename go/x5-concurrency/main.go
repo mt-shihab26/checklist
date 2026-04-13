5861fe0ea0225056c274baa19e002eb98f4710fd
Shihab Mahamud <shihab4t@gmail.com>
2026-04-13 23:37:38 +0600

    order

--- go/x5-concurrency/main.go
+++ go/x5-concurrency/main.go
@@ -8,24 +8,17 @@ import (
 
 func main() {
 	orders := order.Generates(20)
-
 	waitGroup := sync.WaitGroup{}
-
 	waitGroup.Go(func() {
 		order.UpdateOrdersStatus(1, orders)
 	})
-
 	waitGroup.Go(func() {
 		order.UpdateOrdersStatus(2, orders)
 	})
-
 	waitGroup.Go(func() {
 		order.UpdateOrdersStatus(3, orders)
 	})
-
 	waitGroup.Wait()
-
 	order.ReportOrderStatus(orders)
-
 	fmt.Println("All operations completed. Exiting.")
 }
--- go/x5-concurrency/order/order.go
+++ go/x5-concurrency/order/order.go
@@ -15,34 +15,28 @@ type Order struct {
 
 func Generates(count int) []*Order {
 	orders := []*Order{}
-
 	for i := range count {
 		orders = append(orders, &Order{ID: i + 1, Status: "pending"})
 	}
-
 	return orders
 }
 
 func UpdateOrdersStatus(no int, orders []*Order) {
 	for _, order := range orders {
 		time.Sleep(time.Duration(rand.IntN(500) * int(time.Millisecond)))
-
 		status := []string{"processing", "shipped", "delivered"}[rand.IntN(3)]
-
+		order.Mutex.Lock()
 		order.Status = status
-
+		order.Mutex.Unlock()
 		fmt.Printf("Routine #%d: Updated order %d status: %s\n", no, order.ID, status)
 	}
 }
 
 func ReportOrderStatus(orders []*Order) {
 	time.Sleep(1 * time.Second)
-
 	fmt.Printf("--- Order Status Report ---\n")
-
 	for _, order := range orders {
 		fmt.Printf("Order %d: %s\n", order.ID, order.Status)
 	}
-
 	fmt.Println("----------------------------")
 }
