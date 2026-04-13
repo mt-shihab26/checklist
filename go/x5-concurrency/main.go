876d145e0d88ad3f77f0d3bc13da60128c907603
Shihab Mahamud <shihab4t@gmail.com>
2026-04-13 23:42:00 +0600

    wip

--- go/x5-concurrency/main.go
+++ go/x5-concurrency/main.go
@@ -1,24 +1,59 @@
 package main
 
 import (
-	"concurrency/order"
 	"fmt"
+	"math/rand/v2"
 	"sync"
+	"time"
 )
 
 func main() {
-	orders := order.Generates(20)
+	orders := generatesOrders(20)
 	waitGroup := sync.WaitGroup{}
 	waitGroup.Go(func() {
-		order.UpdateOrdersStatus(1, orders)
+		updateOrdersStatus(1, orders)
 	})
 	waitGroup.Go(func() {
-		order.UpdateOrdersStatus(2, orders)
+		updateOrdersStatus(2, orders)
 	})
 	waitGroup.Go(func() {
-		order.UpdateOrdersStatus(3, orders)
+		updateOrdersStatus(3, orders)
 	})
 	waitGroup.Wait()
-	order.ReportOrderStatus(orders)
+	reportOrderStatus(orders)
 	fmt.Println("All operations completed. Exiting.")
+}
+
+type Order struct {
+	ID     int
+	Status string
+	Mutex  sync.Mutex
+}
+
+func generatesOrders(count int) []*Order {
+	orders := []*Order{}
+	for i := range count {
+		orders = append(orders, &Order{ID: i + 1, Status: "pending"})
+	}
+	return orders
+}
+
+func updateOrdersStatus(no int, orders []*Order) {
+	for _, order := range orders {
+		time.Sleep(time.Duration(rand.IntN(500) * int(time.Millisecond)))
+		status := []string{"processing", "shipped", "delivered"}[rand.IntN(3)]
+		order.Mutex.Lock()
+		order.Status = status
+		order.Mutex.Unlock()
+		fmt.Printf("Routine #%d: Updated order %d status: %s\n", no, order.ID, status)
+	}
+}
+
+func reportOrderStatus(orders []*Order) {
+	time.Sleep(1 * time.Second)
+	fmt.Printf("--- Order Status Report ---\n")
+	for _, order := range orders {
+		fmt.Printf("Order %d: %s\n", order.ID, order.Status)
+	}
+	fmt.Println("----------------------------")
 }
